16908674959b288a76d512b0e9742f8a0510a316
KomilT <kaa.96t@gmail.com>
2021-03-07 03:58:19 +0500

    server: categories feature

---
+++ server/features/categories/categoriesController.js
@@ -0,0 +1,59 @@
+const { CustomError } = require("@lib/errors");
+const { Category } = require("@lib/sequelize");
+
+exports.getCategories = async (req, res) => {
+  const categories = await Category.findAll();
+
+  res.status(200).json(categories);
+};
+
+exports.createCategory = async (req, res) => {
+  const { name } = req.body;
+
+  const category = await Category.create({
+    name,
+  });
+
+  res.status(201).json(category);
+};
+
+exports.getCategory = async (req, res) => {
+  const { id } = req.params;
+  const category = await Category.findByPk(id);
+
+  if (!category) {
+    throw new CustomError(404, `Category with id ${id} not found`);
+  }
+
+  res.status(200).json(category);
+};
+
+exports.updateCategory = async (req, res) => {
+  const { id } = req.params;
+  const category = await Category.findByPk(id);
+
+  if (!category) {
+    throw new CustomError(404, `Category with id ${id} not found`);
+  }
+
+  const { name } = req.body;
+
+  await category.update({
+    name,
+  });
+
+  res.status(200).json(category);
+};
+
+exports.deleteCategory = async (req, res) => {
+  const { id } = req.params;
+  const category = await Category.findByPk(id);
+
+  if (!category) {
+    throw new CustomError(404, `Category with id ${id} not found`);
+  }
+
+  await category.destroy();
+
+  res.status(204).json();
+};
