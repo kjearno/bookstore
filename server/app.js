ad78409b77a5a2a7e404fae2fe4f22b8d791b905
KomilT <kaa.96t@gmail.com>
2021-03-07 04:16:04 +0500

    server: update core structure

---
+++ server/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+require("express-async-errors");
+require("module-alias/register");
+
+const { authRouter } = require("@features/auth");
+const { authorsRouter } = require("@features/authors");
+const { booksRouter } = require("@features/books");
+const { categoriesRouter } = require("@features/categories");
+const { usersRouter } = require("@features/users");
+
+const { errorHandler } = require("@lib/errors");
+
+const app = express();
+
+// 1) Global middleware
+app.use(cookieParser());
+app.use(express.json());
+
+// 2) API routes
+app.use("/api/auth", authRouter);
+app.use("/api/authors", authorsRouter);
+app.use("/api/books", booksRouter);
+app.use("/api/categories", categoriesRouter);
+app.use("/api/users", usersRouter);
+
+app.use(errorHandler);
+
+module.exports = app;
