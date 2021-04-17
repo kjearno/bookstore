82747123e17e6345bc29ba946e9b3c8a43f7170f
KomilT <kaa.96t@gmail.com>
2021-04-17 05:51:15 +0500

    client: deployment system

--- client/src/app/index.jsx
+++ client/src/app/index.jsx
@@ -7,7 +7,7 @@ import "./style.scss";
 
 export function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <ScrollToTop />
       <ToastProvider autoDismiss>
         <Pages />
