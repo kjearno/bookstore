022bf1c3cb4370640d7b04ab0cb1baffa0e51ec8
kjearno <kaa.96t@gmail.com>
2022-12-14 20:26:39 +0500

    Use environment variables

--- src/shared/lib/axios.js
+++ src/shared/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://bookstore-73510.herokuapp.com/api",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export { instance as axios };
